14511123f56cadf3b8c8e6697cbf4a2454c2adb8
roberto-palanga <r.palanga94@gmail.com>
2024-03-11 13:01:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppNav from "./components/AppNav.vue";
+
+export default{
+
+  components:{
+    AppNav,
+  }
+}
+
+</script>
+
+<template>
+  <div class="my-contianer">
+    <AppNav/>
+  </div>
+  
+  
+</template>
+
+<style lang="scss">
+@use "./newstyles/styles.scss" as *;
+
+
+
+
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default{
+    name:"AppNav",
+}
+
+</script>
+
+<template>
+    <h1>si comincia</h1>
+  
+</template>
+
+<style>
+
+</style>
