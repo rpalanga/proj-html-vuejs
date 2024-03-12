07be215fef5384f0f5b2c31782aaf934acef4194
roberto-palanga <r.palanga94@gmail.com>
2024-03-12 19:03:47 +0100

    Primo slider finito

--- src/components/AppCarousel.vue
+++ src/components/AppCarousel.vue
@@ -32,16 +32,16 @@ export default {
         </div>
 
         <!-- Carosello con cinque elementi -->
-
-        <div id="carouselExample" class="carousel slide mb-3 position-relative  ">
+       
+        <div id="carouselExample" class="carousel slide mb-3 position-relative ">
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div class="d-flex ">
                         <img src="/public/images/blue_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
-                        <img src="/public/images/blue_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
-                        <img src="/public/images/blue_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
-                        <img src="/public/images/blue_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
-                        <img src="/public/images/blue_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
+                        <img src="/public/images/hipster_black_top-231x300.jpg" class="d-block w-20" alt="...">
+                        <img src="/public/images/spring_printed_dress-231x300.jpg" class="d-block w-20" alt="...">
+                        <img src="/public/images/black_elegant_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
+                        <img src="/public/images/black_leather_jacket-231x300.jpg" class="d-block w-20" alt="...">
                     </div>
                 </div>
                 <div class="carousel-item">
@@ -55,11 +55,11 @@ export default {
                 </div>
             </div>
 
-            <button class="carousel-control-prev position-absolute top-50 start-0 translate-middle" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon text-black" aria-hidden="true"></span>
+            <button class="carousel-control-prev my-position-absolute-left " type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon btn-black" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next position-absolute top-50 start-100 translate-middle" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <button class="carousel-control-next my-position-absolute-right" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden ">Next</span>
             </button>
@@ -74,4 +74,29 @@ export default {
 
 <style lang="scss">
 @use "../newstyles/styles.scss" as *;
+
+.carousel-control-prev,
+.carousel-control-next {
+  background-color: #000000;
+}
+
+.my-position-absolute-left{
+    position: absolute;
+    
+    top: 100px;
+    left: -110px;
+    bottom: 100px;
+    width: 10%;
+}
+.my-position-absolute-right{
+    position: absolute;
+    
+    top: 100px;
+    right: -110px;
+    bottom: 100px;
+    width: 10%;
+
+}
+
+
 </style>
